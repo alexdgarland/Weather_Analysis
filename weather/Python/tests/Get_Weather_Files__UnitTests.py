63f7c8a356ab862eb331bd5d4a84a89e0a8c76f4
Alex Garland <alex.d.garland@gmail.com>
2014-12-12 11:27:45 +0000

    Unit test for response conversion

--- weather/Python/tests/Get_Weather_Files__UnitTests.py
+++ weather/Python/tests/Get_Weather_Files__UnitTests.py
@@ -1,11 +1,13 @@
 #!/usr/bin/python
 
 import unittest
+import sys
 
 import testhelper as t
 t.register_maincodefolder()
 # Get module under test
 import Get_Weather_Files as gwf
+
 
 class Tests_ParseContentTypeForHTMLText(unittest.TestCase):
     
@@ -28,6 +30,45 @@ class Tests_ParseContentTypeForHTMLText(unittest.TestCase):
         self.assertFalse(istexthtml)
         self.assertEqual(encoding, None)
 
+
+class MockHTTPResponse(object):
+    
+    def _UTF8String(self, string):
+        if sys.version_info.major >= 3:
+            return string
+        else:
+            return(unicode(string, 'utf-8'))    
+    
+    def __init__(self, contenttype, bodytext):
+        self._contenttype = contenttype
+        self._utf8_bodytext = self._UTF8String(bodytext)
+    def getheader(self, name):
+        if name == 'Content-Type':
+            return self._contenttype
+        else:
+            return None
+    def read(self):
+        return self._utf8_bodytext.encode('utf-8')
+
+
+class Tests_GetHTMLTextFromResponse(unittest.TestCase):
+    
+    def test_htmlresponse_getstext(self):
+        inputtext = '<html></html>'
+        response = MockHTTPResponse('text/html', inputtext)
+        outputtext = gwf.GetHTMLTextFromResponse(response)
+        self.assertEqual(inputtext, outputtext)
+            
+    def test_nonhtmlresponse_raisesexception(self):
+        response = MockHTTPResponse('text/xml', '<xml></xml>')
+        thrownexception = None # Initial state
+        try:
+            outputtext = gwf.GetHTMLTextFromResponse(response)
+        except Exception as e:
+            thrownexception = e
+        self.assertEqual(type(thrownexception), type(Exception()))
+        self.assertEqual(str(thrownexception),
+                         'Response content is not TEXT/HTML.')
 
 
 if __name__ == '__main__':
